cf1cb3856842ec9735b60a8debcbf3b3816ab457
Aluno <jessica.hahn.silva@gmail.com>
2018-09-14 11:04:31 -0300

    continua Q1

--- T2/Q1/dss_parte1.m
+++ T2/Q1/dss_parte1.m
@@ -12,7 +12,7 @@ N = 10; % Numero de chips por bit de informacao
 fc = 40e3; % frequencia da modulacao BPSK
 Rb = 1e3; % taxa de bits
 amostra_chip = 100; % Numero de amostras por chip
-
+fc = 40e3; %freq portadora
 x_t = randi([0 1],1,Nb)*2-1;
 c_t = randi([0 1],1,L)*2-1;
 up = amostra_chip*N; % numero de amostras por bit
@@ -20,7 +20,7 @@ fa = Rb*up; % frequencia de amostragem (numero de amostras por segundo)
 dur = Nb/Rb;
 Ta = 1/fa;
 t = 0:Ta:dur-Ta; %tempo
-
+pt = cos(2*pi*fc*t);
 %superamostragem
 filtro = ones(1,up);
 x_t_up = filter(filtro,1, upsample(x_t,up));
@@ -28,8 +28,34 @@ filtro_c = ones(1,amostra_chip);
 c_t_up = filter(filtro_c,1, upsample(c_t,amostra_chip));
 c_t_up = repmat(c_t_up,1,length(x_t_up)/length(c_t_up));
 s_t = x_t_up.*c_t_up;
+psk = s_t.*pt;
 
-%plot
-% subplot(4,1,1); plot(x_t)
-% subplot(4,1,2); plot(c_t)
-% subplot(4,1,3); plot(s_t)
+%ver melhor vetor de frequencia
+%frequencia
+vetor = [-length(t)/2:length(t)/2-1];
+f = vetor*fa/2;
+X = fft(x_t_up);
+C = fft(c_t_up);
+S = fft(s_t);
+PSK = fft(psk);
+%tempo
+figure()
+subplot(411);
+plot(t,x_t_up,'r');
+title('Sinal x(t)')
+xlim([0 4*1/up]);
+subplot(412);
+plot(t,c_t_up);
+title('Sinal c(t)')
+xlim([0 4*1/up]);
+subplot(413);
+plot(t,s_t,'g');
+xlim([0 4*1/up]);
+title('Sinal s(t)')
+subplot(414);
+plot(t,psk,'k');
+xlim([0 4*1/up]);
+title('Sinal s_b_p_s_k_(_t_)');
+
+figure();
+plot(f,fftshift(abs(PSK))); 
