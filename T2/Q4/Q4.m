c5f2dc6773294aa8fb462024d5c4d232da506e52
Aluno <jessica.hahn.silva@gmail.com>
2018-09-21 09:37:48 -0300

    continuacao Q4

--- T2/Q4/Q4.m
+++ T2/Q4/Q4.m
@@ -1,7 +1,22 @@
-taps = [1 3]; %?
-estado = [1 0 0 ]; %nao pode ser [0 0 0] %estado inicial
-m = 7; % número de bits de estado ?
+%obs matlab 2012 da problema
+clear all;
+close all;
+m = 7;
+Nb = 100000; % Numero de bits transmitidos
+L = 2.^m - 1; % Periodo do pseudo-codigo
+N = L; % Numero de chips por bit de informacao
+estado_inicial = [0 0 0 0 0 0 1]; %igual ao tamanho de L, estado inicial qualquer
+h =  [2 -0.5 0.5]; %canal
+SNR = 10;
+x = kron(randi([0 1],1,Nb)*2-1,ones([1,L]));
 
 objetoLFSR = LFSR;
 %geracao de codigo, lembrando que o codigo é ciclico
-codigo = objetoLFSR.lfsr(estado,taps,m)
+for i=1:3
+        taps = objetoLFSR.rand_vetor(i);
+        %geracao de codigo, lembrando que o codigo é ciclico
+        codigos{i} = objetoLFSR.lfsr(estado_inicial,taps,m)
+        codigos{i} = repmat(codigos{i},1,Nb); 
+        
+end
+
