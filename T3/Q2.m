877af4f8f40cd0f1b46f3a4eca5ae497827d1dc8
jessica.hahn <jessica.hahn.silva@gmail.com>
2018-11-03 20:18:34 -0300

    T3 finalizado

--- T3/Q2.m
+++ T3/Q2.m
@@ -1,19 +1,19 @@
-% Código de Hamming
+% Codigo de Hamming
 codigo.n = 7;
 codigo.k = 4;
-% Taxa do código
+% Taxa do codigo
 R = codigo.k/codigo.n;
 % Matriz geradora
 Ik = eye(k,k);
 p = [1 1 0;1 0 1;0 1 1;1 1 1];
 codigo.G = [Ik,p];
-% Palavras código
+% Palavras codigo
 u_decimal = [0:2^codigo.k-1];
 codigo.u = de2bi(u_decimal,codigo.k,2,'left-msb');
-% Código de bloco
+% Codigo de bloco
 C = mod(codigo.u*codigo.G,2);
 codigo.c = (C*2)-1;
-% Informação: b
+% Informacao: b
 b = randi([0 1], 1,codigo.n)
 b_polar = (b*2)-1;
 r = awgn(b_polar,1);
--- T3/Sindrome.m
+++ T3/Sindrome.m
@@ -4,15 +4,16 @@ function vetor_funcoes = Sindrome
 end
 
 function [u_hat] = HDD(b,struct_info)
-    k = struct_info.k
-    lut = struct_info.lut
-    h = struct_info.h
-    s = mod(b*h',2)
-    [l,c] = size(lut)
+    k = struct_info.k;
+    lut = struct_info.lut;
+    h = struct_info.h;
+    s = mod(b*h',2);
+    [l,c] = size(lut);
+    u_hat = 0;
     for i=1:l
        if(s == lut(i,1:k-1))
-          erro = lut(i,(length(s))+1:end)
-          palavra_codigo = xor(erro,b)
+          erro = lut(i,(length(s))+1:end);
+          palavra_codigo = xor(erro,b);
           u_hat = palavra_codigo(1:k);
           break
        end
@@ -21,9 +22,9 @@ end
 
 function [u_hat] = SDD(r,struct_info)
     c = struct_info.c;
-    k = struct_info.k
+    k = struct_info.k;
     dist = r * c';
     [M, index] = max(dist);
     c_hat = (c(index,:)+1)/2;
-    u_hat = c_hat(1:k)
+    u_hat = c_hat(1:k);
 end
